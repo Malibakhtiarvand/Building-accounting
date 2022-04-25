f724dc841b662d8aad5c86d5b62b97b4102cff2a
mohammad ali bakhtiarvand <alibakhtiarvand1386@gmail.com>
2022-04-25 21:23:38 +0430

    make Cost settings

--- RevenuesAnExpenses.py
+++ RevenuesAnExpenses.py
@@ -1,6 +1,7 @@
 from tkinter import*
 
 widnow=Tk()
+widnow.title('Revenues an expenses')
 widnow.geometry('300x400')
 widnow.configure(bg='red')
 
@@ -14,6 +15,22 @@ tozih_input=Text(widnow,height=4)
 tozih_input.pack()
 
 
+# def checked(check:bool) -> bool:
+#     return check
+
+def writeCostAndIcomeInFile ():
+    ischeckedCost = cost_intVar.get()
+    ischeckedIcome = Income_intVar.get()
+    if ischeckedCost == 1:
+        with open('money.txt','r+') as f:
+            readFile=int(f.readlines()[-1])
+            print(readFile)
+            f.write('\n')
+            f.write(str((readFile)-int(countMoney.get())))
+
+
+
+
 cost_intVar=IntVar()
 Income_intVar=IntVar()
 Cost=Checkbutton(widnow,text='هزینه',variable=cost_intVar,offvalue=0,onvalue=1)
@@ -23,7 +40,11 @@ Cost.pack()
 Income=Checkbutton(widnow,text='درآمد',variable=Income_intVar,offvalue=0,onvalue=1)
 Income.pack()
 
-finish=Button(widnow,text='انجام').pack(side='bottom')
+countMoneyLabel = Label(widnow,text='مقدار').pack()
+countMoney = Entry(widnow)
+countMoney.pack(fill='x')
+
+finish=Button(widnow,text='انجام',command=writeCostAndIcomeInFile).pack(side='bottom')
 
 
 
